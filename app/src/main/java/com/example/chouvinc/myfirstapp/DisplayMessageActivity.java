d0f320f3938936c2d45a66570b8c4130775b76cb
Vincent Chou <vin.chou@mail.utoronto.ca>
2018-01-31 21:19:13 -0500

    Building Your First App

--- app/src/main/java/com/example/chouvinc/myfirstapp/DisplayMessageActivity.java
+++ app/src/main/java/com/example/chouvinc/myfirstapp/DisplayMessageActivity.java
@@ -1,7 +1,9 @@
 package com.example.chouvinc.myfirstapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class DisplayMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
+
+        // get the intent of the current activity instance
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // get the textView with id textView and set the message
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
     }
+
+
 }
